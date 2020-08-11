d70faacb33809f0cf4f3bb912fbf0f742a9719ea
pepeworm <53125684+impyworm@users.noreply.github.com>
2020-08-11 12:36:22 -0600

    Prepare for typewriter effect

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+// Navbar scroll color change
+
 const navbar = document.querySelector("nav");
 
 window.onscroll = () => {
@@ -8,3 +10,16 @@ window.onscroll = () => {
         navbar.classList.remove("navbar-scroll");
     }
 };
+
+// Description text writer effect
+
+const textDelete = document.querySelector(".text-delete");
+const descriptionWords = ["Web Developer", "Bot Developer"]
+
+window.onload = () => {
+    textDelete.textContent = descriptionWords[0];
+}
+
+(function typeWriter() {
+    for (let i = 0; i < )
+})();
