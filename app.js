11ea34447988d64979e932f34567792f3c8790bd
pepeworm <impyworm@gmail.com>
2021-04-10 09:31:13 -0600

    Remove MailChimp

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-
+require("dotenv").config();
 const express = require("express");
 const https = require("https");
 const request = require("request");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -17,54 +18,46 @@ app.post("/", (req, res) => {
     const lastName = req.body.userLastName;
     const email = req.body.userEmail;
 
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName,
-                },
-            },
-        ],
-    };
-
-    const jsonData = JSON.stringify(data);
-
-    const dc = "us17";
-    const listId = "bd3c5ea890";
-
-    const url = `https://${dc}.api.mailchimp.com/3.0/lists/${listId}`;
-
-    const options = {
-        method: "POST",
-        auth: "pepeworm:1f508f7f38bafcc99c7683135bad50e7-us17",
-    };
-
-    const request = https.request(url, options, (response) => {
-        if (response.statusCode === 200) {
-            res.sendFile(`${__dirname}/success.html`);
-        } else {
-            res.sendFile(`${__dirname}/error.html`);
-        }
-
-        response.on("data", (data) => {
-            console.log(JSON.parse(data));
-        });
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        },
     });
 
-    request.write(jsonData);
-    request.end();
+    const message = {
+        from: `Pepeworm <${process.env.EMAIL}>`,
+        to: email,
+        subject: `Hello ${firstName} ${lastName}!`,
+        html: `<p>Pepeworm here, how are you doing?</p>`,
+    };
+
+    transporter.sendMail(message, (err) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/error");
+        } else {
+            res.redirect("/success");
+        }
+    });
 });
 
-app.post("/error", (req, res) => {
-    res.redirect("/#contact");
-});
+app.route("/error")
+    .get((req, res) => {
+        res.sendFile(`${__dirname}/error.html`);
+    })
+    .post((req, res) => {
+        res.redirect("/#contact");
+    });
 
-app.post("/success", (req, res) => {
-    res.redirect("/#contact");
-});
+app.route("/success")
+    .get((req, res) => {
+        res.sendFile(`${__dirname}/success.html`);
+    })
+    .post((req, res) => {
+        res.redirect("/#contact")
+    });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port 3000");
